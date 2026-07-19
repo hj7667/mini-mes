b9e5af4efbe0485cd665a450ed8523d6316475e0
lucy <chohj.biz@gmail.com>
2026-07-19 13:47:16 +0900

    create

---
+++ java-basic/src/Main.java
@@ -0,0 +1,18 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        boolean completed = false;
+        String status = "";
+
+        for (int i = 1; i <= 3; i++) {
+
+            String lotNo = "LOT-00" + i;
+
+            System.out.println(lotNo);
+
+        }
+
+    }
+
+}
