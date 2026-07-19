5abae10902732896091de325fc6da6b2ec3546b4
lucy <chohj.biz@gmail.com>
2026-07-19 13:55:15 +0900

    feat
    test

--- java-basic/src/Main.java
+++ java-basic/src/Main.java
@@ -1,17 +1,29 @@
 public class Main {
 
+    public static void printLot(String workOrderNo, int quantity) {
+
+        System.out.println("LOT 번호  : " + workOrderNo);
+        System.out.println("수량 : " + quantity);
+
+    }
+
+
+/*
+LOT 번호 : LOT-001
+수량 : 500
+
+LOT 번호 : LOT-002
+수량 : 300
+
+LOT 번호 : LOT-003
+수량 : 150
+ */
+
     public static void main(String[] args) {
 
-        boolean completed = false;
-        String status = "";
-
-        for (int i = 1; i <= 3; i++) {
-
-            String lotNo = "LOT-00" + i;
-
-            System.out.println(lotNo);
-
-        }
+          printLot("LOT-001", 500);
+          printLot("LOT-002", 300);
+          printLot("LOT-003", 150);
 
     }
 
