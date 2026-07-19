64bbc87ab38cf08e6ba2f0d7951381199d5915e4
lucy <chohj.biz@gmail.com>
2026-07-19 13:55:15 +0900

    feat
    객체 생성연습
    
    # Conflicts:
    #	java-basic/src/Main.class
    #	java-basic/src/Main.java
    
    # Conflicts:
    #	java-basic/src/WorkOrder.java

--- java-basic/src/WorkOrder.java
+++ java-basic/src/WorkOrder.java
@@ -1,8 +1,18 @@
-
 public class WorkOrder {
 
-      String workOrderNo;
-      int quantity;
-      boolean completed;
+    String workOrderNo;
+    int quantity;
 
-  }
+    // 생성자
+    public WorkOrder(String workOrderNo, int quantity) {
+        this.workOrderNo = workOrderNo;
+        this.quantity = quantity;
+    }
+
+    // 메서드
+    public void printInfo() {
+        System.out.println("생산지시번호 : " + workOrderNo);
+        System.out.println("생산수량 : " + quantity);
+    }
+
+}
